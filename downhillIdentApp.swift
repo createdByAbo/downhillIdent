5ffe1f4535c80eb62cd3dbd629b43e2d0384b061
Daniel Krupa <abo@Daniels-MacBook-Air.local>
2024-07-23 19:51:50 +0200

    initial commit

---
+++ downhillIdentApp.swift
@@ -0,0 +1,17 @@
+//
+//  downhillIdentApp.swift
+//  downhillIdent
+//
+//  Created by Daniel Krupa on 23/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct downhillIdentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
